b6876be302aab54f22a398c17be6c3c4f2813471
Cassidy Laidlaw <cassidy256@gmail.com>
2023-11-03 13:27:36 -0700

    Add test for toy experiments

---
+++ tests/test_toy_experiments.py
@@ -0,0 +1,16 @@
+
+import subprocess
+
+def test_toy_experiments(tmp_path):
+    subprocess.check_call(
+        [
+            "python",
+            "-m",
+            "inconsistent_preferences.toy_experiments",
+            "--env=1d",
+            "--batch_size=8",
+            "--lr=1e-4",
+            "--num_iterations=5",
+            f"--out_dir={tmp_path}",
+        ],
+    )
